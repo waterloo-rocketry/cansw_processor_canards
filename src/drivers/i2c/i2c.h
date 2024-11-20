fd19b08bf772acfdab3fb4d820c6790be176df73
taisirhassan <taisirisawesome@gmail.com>
2024-11-19 23:39:45 -0500

    Refactored I2C header to include common rocketlib and update status type accordingly for each function

--- src/drivers/i2c/i2c.h
+++ src/drivers/i2c/i2c.h
@@ -1,7 +1,7 @@
 #ifndef I2C_H
 #define I2C_H
 
-#include "stm32h7xx_hal.h"
+#include "src/third_party/rocketlib/include/common.h"
 #include <stdint.h>
 
 typedef enum {
@@ -11,21 +11,12 @@ typedef enum {
     I2C_PERIPHERAL_COUNT // Number of peripherals
 } i2c_peripheral_t;
 
-typedef enum {
-    I2C_OK = 0, // Operation completed successfully
-    I2C_ERROR, // General error occurred
-    I2C_BUSY, // Bus or device busy
-    I2C_TIMEOUT, // Operation timed out
-    I2C_NACK // Device did not acknowledge
-} i2c_status_t;
-
 // Must be called before RTOS scheduler starts
-i2c_status_t i2c_init(i2c_peripheral_t periph, uint32_t timeout_ms);
+w_status_t i2c_init(i2c_peripheral_t periph, uint32_t timeout_ms);
 
 // Read len bytes from reg into data buffer, max 255 byte
-i2c_status_t i2c_read_reg(i2c_peripheral_t periph, uint8_t reg, uint8_t *data, uint16_t len);
+w_status_t i2c_read_reg(i2c_peripheral_t periph, uint8_t reg, uint8_t *data, uint16_t len);
 
-// Write len bytes from data buffer to reg, max 255 bytes
-i2c_status_t i2c_write_reg(i2c_peripheral_t periph, uint8_t reg, uint8_t *data, uint16_t len);
+w_status_t i2c_write_reg(i2c_peripheral_t periph, uint8_t reg, uint8_t *data, uint16_t len);
 
 #endif // I2C_H
