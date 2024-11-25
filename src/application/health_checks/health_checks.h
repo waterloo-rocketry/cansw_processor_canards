03ccbe9b1ea217a41a28d28ef973b2d72461603c
Kimberley Hoang <kimberley.hoang528@gmail.com>
2024-11-24 19:38:19 -0500

    fix formatting

--- src/application/health_checks/health_checks.h
+++ src/application/health_checks/health_checks.h
@@ -16,8 +16,8 @@
 #define R_SENSE 0.033
 #define INA180A3_GAIN 100.0
 
-#define ADC1_VOLTAGE_V(counts)(counts / ADC1_MAX_COUNTS * ADC1_VREF)
-#define ADC1_CURR_mA(voltage) (voltage / INA180A3 / R_SENSE )
+#define ADC1_VOLTAGE_V(counts) (counts / ADC1_MAX_COUNTS * ADC1_VREF)
+#define ADC1_CURR_mA(voltage) (voltage / INA180A3 / R_SENSE)
 
 #define MAX_CURR_5V_mA 400
 
