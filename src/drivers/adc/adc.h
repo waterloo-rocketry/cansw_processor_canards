c77f8cf8f77317bdfe060e5cf70085cf58af4cb0
jerryjcr <jerry.jia295@gmail.com>
2024-11-26 16:10:12 -0500

    format update

--- src/drivers/adc/adc.h
+++ src/drivers/adc/adc.h
@@ -4,22 +4,22 @@
 #include <stdbool.h>
 
 typedef enum {
-	PROCESSOR_BOARD_VOLTAGE = 0,
-	ADC_CHANNEL_2,
-	ADC_CHANNEL_3,
-	ADC_CHANNEL_4,
-	ADC_CHANNEL_5
+    PROCESSOR_BOARD_VOLTAGE = 0,
+    ADC_CHANNEL_2,
+    ADC_CHANNEL_3,
+    ADC_CHANNEL_4,
+    ADC_CHANNEL_5
 } adc_channel_t;
 
 w_status_t adc_init(void);
 
-//writes adc value of the specified channel to the output parameter
+// writes adc value of the specified channel to the output parameter
 w_status_t adc_get_value(adc_channel_t channel, uint32_t *output);
 
-//writes processor board voltage to the output parameter
+// writes processor board voltage to the output parameter
 w_status_t adc_get_board_voltage(uint16_t *output);
 
-//writes processor board voltage to the output parameter
+// writes processor board voltage to the output parameter
 w_status_t adc_get_board_current(uint16_t *output);
 
 #endif
