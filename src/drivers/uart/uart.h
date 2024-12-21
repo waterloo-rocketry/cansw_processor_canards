b76d96e429d75c9fa4315b57cf65ccccffdb7efd
punchdii <sunqiche@outlook.com>
2024-12-20 21:53:56 -0500

    applied clang formatting

--- src/drivers/uart/uart.h
+++ src/drivers/uart/uart.h
@@ -8,7 +8,6 @@ typedef enum
 {
 	UART_IMU;		   // Movella IMU
 	UART_DEBUG_SERIAL; // debugger serial
-
 }
 uart_device_t;
 
@@ -18,3 +17,4 @@ w_status_t uart_init(uart_device_t device, uint32_t baud, uint32_t timeout_ms);
 w_status_t uart_write(uart_device_t device, const uint8_t *data, uint8_t len);
 
 #endif // UART_H
+src\drivers\uart\uart.h
