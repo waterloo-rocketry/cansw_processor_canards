29e1c86391e14d47bcda80b0154326bca20ee7c2
Jacob Gordon <jacobgordon1003@gmail.com>
2024-12-21 21:08:52 -0500

    Added the status code as returns

--- src/application/can_handler/can_handler.h
+++ src/application/can_handler/can_handler.h
@@ -1,6 +1,7 @@
 #ifndef __CAN_HANDLER
 #define __CAN_HANDLER
 
+#include "common.h"
 #include "canlib.h"
 #include "stm32h7xx_hal.h"
 #include "FreeRTOS.h"
@@ -10,9 +11,11 @@ extern FDCAN_HandleTypeDef hfdcan1;
 extern xQueueHandle busQueue_tx;
 extern xQueueHandle busQueue_rx;
 
-void can_handle_rx(consst can_msg_t *message, uint32_t timestamp);
-void can_handle_tx(consst can_msg_t *message);
+//Called by other tasks to send to can
+w_status_t can_handle_tx(consst can_msg_t *message);
 
+//Used by main to init
+w_status_t can_handle_rx(consst can_msg_t *message, uint32_t timestamp);
 void canHandlerTask_rx(void *argument);
 void canHandlerTask_tx(void *argument);
 bool canHandlerInit(void); 
