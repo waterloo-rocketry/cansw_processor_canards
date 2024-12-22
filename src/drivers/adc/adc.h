c9c3ff2d20fdf588331a2affa7ac74637230a496
jerryjcr <jerry.jia295@gmail.com>
2024-12-22 13:14:07 -0700

    removed the get_voltage and get_current functions, and added comments

--- src/drivers/adc/adc.h
+++ src/drivers/adc/adc.h
@@ -5,21 +5,13 @@
 
 typedef enum {
     PROCESSOR_BOARD_VOLTAGE = 0,
-    ADC_CHANNEL_2,
-    ADC_CHANNEL_3,
-    ADC_CHANNEL_4,
-    ADC_CHANNEL_5
 } adc_channel_t;
 
 w_status_t adc_init(void);
 
 // writes adc value of the specified channel to the output parameter
+// blocks until adc conversion is finished, or times out after 1 ms
+// is thread safe: semaphores prevent multiple conversions from happening at once
 w_status_t adc_get_value(adc_channel_t channel, uint32_t *output);
-
-// writes processor board voltage to the output parameter
-w_status_t adc_get_board_voltage(uint16_t *output);
-
-// writes processor board voltage to the output parameter
-w_status_t adc_get_board_current(uint16_t *output);
 
 #endif
