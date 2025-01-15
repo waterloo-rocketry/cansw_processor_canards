0375b5dc4e2479903afb20c18b32075ff57c4b28
ammmohib <ammmohib@uwaterloo.ca>
2025-01-15 08:01:40 -0500

    fixed formatting issue

--- src/drivers/timer/timer.h
+++ src/drivers/timer/timer.h
@@ -1,9 +1,9 @@
 #ifndef TIMER_H
 #define TIMER_H
 
-//tracks system time since program startup
-//returns time passed in the form of clock ticks
-//each clock tick is equivalent to 0.1ms
+// tracks system time since program startup
+// retrieves time passed in the form of clock ticks
+// timer resolution set to 0.1ms (10000Hz frequency)
 w_status_t timer_get_ms();
 
 #endif
