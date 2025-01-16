dfed44fb358263c38e7fe64848a6209b8d8ebbbd
taisirhassan <taisirisawesome@gmail.com>
2025-01-15 21:50:38 -0500

    refactor i2c.h to remove deprecated device definitions and update function signatures for I2C bus interface

--- src/drivers/i2c/i2c.h
+++ src/drivers/i2c/i2c.h
@@ -12,38 +12,6 @@
 #define I2C_H
 
 #include "rocketlib/include/common.h" // Common definitions
-<<<<<<< HEAD
-#include <stdint.h> // Standard integer types
-
-// Supported I2C devices
-typedef enum {
-    I2C_DEVICE_POLULU, // Polulu AltIMU-v6
-    I2C_DEVICE_ST, // ST LSM6DSO32 IMU
-    I2C_DEVICE_COUNT // Number of devices
-} i2c_device_t;
-
-// Initialize an I2C device
-// device: I2C device to initialize
-// timeout_ms: Operation timeout in milliseconds
-// Returns: Status code indicating success or failure
-w_status_t i2c_init(i2c_device_t device, uint32_t timeout_ms);
-
-// Read data from a device register
-// device: I2C device
-// reg: Register address
-// data: Buffer to store read data
-// len: Number of bytes to read (max 255)
-// Returns: Status code indicating success or failure
-w_status_t i2c_read_reg(i2c_device_t device, uint8_t reg, uint8_t *data, uint8_t len);
-
-// Write data to a device register
-// device: I2C device
-// reg: Register address
-// data: Data to write
-// len: Number of bytes to write
-// Returns: Status code indicating success or failure
-w_status_t i2c_write_reg(i2c_device_t device, uint8_t reg, const uint8_t *data, uint8_t len);
-=======
 #include <stdint.h>                   // Standard integer types
 
 /**
@@ -115,6 +83,5 @@ w_status_t i2c_read_reg(i2c_bus_t bus, uint8_t device_addr, uint8_t reg, uint8_t
  * @note The device_addr parameter expects the 7-bit address. The R/W bit is handled internally.
  */
 w_status_t i2c_write_reg(i2c_bus_t bus, uint8_t device_addr, uint8_t reg, const uint8_t *data, uint8_t len);
->>>>>>> 225a94a (refactor i2c.h to improve documentation and update I2C bus interface)
 
 #endif // I2C_H
