34a682eb166b102eb12c638612ad3334aefc48a1
Hany25 <YitingHan4@gmail.com>
2025-01-16 20:55:14 -0500

    Update state_estimation.h

--- src/application/state_estimation/state_estimation.h
+++ src/application/state_estimation/state_estimation.h
@@ -1,49 +1,48 @@
-#ifdef STATE_EST_H_
-#define STATE_EST_H_
+#ifndef STATE_EST_H
+#define STATE_EST_H
 
 #include "controller.h"
+#include "can_handler.h"
+#include "common/math/math.h"
+#include "third_party/rocketlib/include/common.h"
 #include <stdbool.h>
 #include <stdint.h>
 
-typedef struct {
+typedef struct
+{
     /* data */
     float commanded_angle;
     uint32_t timestamp;
-} control_output_SE_t;
+} Estimator_control_input_t;
 
-typedef struct {
-    /* data */
-    float x;
-    float y;
-    float z;
-} vectors_3axis;
-
-typedef struct {
+typedef struct
+{
     /* data */
     float timestamp_imu;
-    vectors_3axis accelerometer;
-    vectors_3axis gyroscope;
-    vectors_3axis magnometer;
+    vector3d_t accelerometer;
+    vector3d_t gyroscope;
+    vector3d_t magnometer;
     float barometer;
-} IMU_data;
+} Estimator_IMU_input_data;
 
-typedef struct {
+typedef struct
+{
     /* data */
-    vectors_3axis attitude;
-    vectors_3axis rates;
-    vectors_3axis velocity;
+    vector3d_t attitude;
+    vector3d_t rates;
+    vector3d_t velocity;
     float altitude;
     uint32_t timestamp_con;
 
-} controller_state_t;
+} Estimator_controller_state_t;
 
 #define can_output_rate 3;
 #define can_output_counter 0;
 
-w_status_t StateEst_Update_Inputs_imu(IMU_data *data);
+w_status_t StateEst_Update_Inputs_imu(Estimator_IMU_input_data *data);
 
 void StateEst_task(void *argument);
 
-w_status_t StateEst_Init(controller_state_t *data);
+w_status_t StateEst_Init(Estimator_controller_state_t *data);
 
 #endif
