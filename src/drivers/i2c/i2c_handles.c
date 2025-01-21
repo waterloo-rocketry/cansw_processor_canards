e732fd47fa44c2b6e134366dfb9b74d80c744b32
taisirhassan <taisirisawesome@gmail.com>
2025-01-21 03:38:55 +0000

    Refactor I2C handle definitions to remove dummy initializations in source file

--- src/drivers/i2c/i2c_handles.c
+++ src/drivers/i2c/i2c_handles.c
@@ -1,7 +1,5 @@
 #include "i2c_handles.h"
 
-/* Provide dummy (weak) definitions of the I2C handles.
-   If your firmware later provides proper definitions, you can remove or override these. */
-I2C_HandleTypeDef hi2c1 = {0};
-I2C_HandleTypeDef hi2c2 = {0};
-I2C_HandleTypeDef hi2c3 = {0};
+I2C_HandleTypeDef hi2c1;
+I2C_HandleTypeDef hi2c2;
+I2C_HandleTypeDef hi2c3;
--- src/drivers/i2c/i2c_handles.h
+++ src/drivers/i2c/i2c_handles.h
@@ -3,10 +3,8 @@
 
 #include "stm32h7xx_hal.h"
 
-/* Declare the global I2C handle variables that the driver expects.
-   In a real system these would be defined by CubeMX (or similar configuration code). */
 extern I2C_HandleTypeDef hi2c1;
 extern I2C_HandleTypeDef hi2c2;
 extern I2C_HandleTypeDef hi2c3;
 
-#endif /* I2C_HANDLES_H */
+#endif // I2C_HANDLES_H
