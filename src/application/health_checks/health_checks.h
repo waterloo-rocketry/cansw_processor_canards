0337e9458dc1cc9e69b92517b11b14a454ac7aa4
Kimberley Hoang <kimberley.hoang528@gmail.com>
2025-01-21 02:43:05 -0500

    fix formatting

--- src/application/health_checks/health_checks.h
+++ src/application/health_checks/health_checks.h
@@ -1,10 +1,10 @@
 #ifndef HEALTH_CHECKS_H
 #define HEALTH_CHECKS_H
 
-#include <stdbool.h>
-#include <stdint.h>
 #include "FreeRTOS.h"
 #include "rocketlib/include/common.h"
+#include <stdbool.h>
+#include <stdint.h>
 
 w_status_t health_check_init(void);
 void health_check_task(void *argument);
