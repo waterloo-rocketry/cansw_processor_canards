a4aa4daf2f2fd44fe80eecbb31c3815c0d40aab5
celery6 <71736183+celery6@users.noreply.github.com>
2025-01-23 03:20:51 +0000

    add uart read design

--- src/drivers/uart/uart.h
+++ src/drivers/uart/uart.h
@@ -5,14 +5,24 @@
 #include <stdint.h>
 
 typedef enum {
-    UART_IMU; // Movella IMU
-        UART_DEBUG_SERIAL; // debugger serial
-}
-uart_device_t;
+    UART_MOVELLA, // Movella IMU
+    UART_DEBUG_SERIAL, // debugger serial
+    UART_CHANNEL_COUNT // Number of UART channels
+} uart_channel_t;
 
 // Must be called before RTOS scheduler starts
-w_status_t uart_init(uart_device_t device, uint32_t baud, uint32_t timeout_ms);
+// Initialize the specified UART channel with given timeout value.
+w_status_t uart_init(uart_channel_t device, uint32_t timeout_ms);
 
-w_status_t uart_write(uart_device_t device, const uint8_t *data, uint8_t len);
+// Write to the specified UART channel
+// If multiple tasks write to the same channel this will block while waiting.
+// Returns the status of data transfer
+w_status_t uart_write(uart_channel_t channel, const uint8_t *data, uint8_t len);
+
+// Get latest full msg received by `channel`
+// Will drop msgs if msgs are received faster than they are read
+// Blocks for up to `timeout` ms if no msg is available
+// Returns the status, received msg into `data`, msg length into `len`
+w_status_t uart_read(uart_channel_t channel, uint8_t *data, uint8_t *len, uint32_t timeout);
 
 #endif // UART_H
