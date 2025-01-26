43a0c22674b26b480b5836e1800e039bd15e95d9
Sam <70551337+samcyx@users.noreply.github.com>
2025-01-26 04:27:10 +0000

    completed initialization of handle in uart_int function and the error check of HAL_status

--- src/drivers/uart/uart.c
+++ src/drivers/uart/uart.c
@@ -8,10 +8,11 @@
 /**
  * The data associated with a UART channel
  */
-typedef struct {
+typedef struct
+{
     UART_HandleTypeDef *handle; // HAL uart handle
     SemaphoreHandle_t
-        transfer_complete; // Communicate transfer complete between uart_write and the ISR
+        transfer_complete;         // Communicate transfer complete between uart_write and the ISR
     SemaphoreHandle_t write_mutex; // Allows tasks to line up to use uart_write
 } uart_handle_t;
 
@@ -25,7 +26,8 @@ uart_handle_t uart_channel_map[UART_CHANNEL_COUNT];
 /**
  * The ISR triggered when the `huart` channel has completed a transmission
  */
-void uart_transmit_complete_isr(UART_HandleTypeDef *huart) {
+void uart_transmit_complete_isr(UART_HandleTypeDef *huart)
+{
     // TODO: do stuff...
 }
 
@@ -33,14 +35,27 @@ void uart_transmit_complete_isr(UART_HandleTypeDef *huart) {
 
 // Must be called before RTOS scheduler starts
 // Initialize the specified UART channel with given timeout value.
-w_status_t uart_init(uart_channel_t device, UART_HandleTypeDef *handle) {
+w_status_t uart_init(uart_channel_t device, UART_HandleTypeDef *handle)
+{
     w_status_t status = W_SUCCESS;
 
     // Register the transmit-complete ISR for this UART channel
-    HAL_UART_RegisterCallback(handle, HAL_UART_TX_COMPLETE_CB_ID, uart_transmit_complete_isr);
+    HAL_StatusTypeDef HAL_status = HAL_UART_RegisterCallback(handle, HAL_UART_TX_COMPLETE_CB_ID, uart_transmit_complete_isr);
     // TODO: check the return status of registerCallback and handle errors
-
+    if (HAL_status != HAL_OK)
+    {
+        status = W_FAILURE; // error occured in HAL_UART_RegisterCallback function
+    }
     // TODO: init other stuff ...
+    handle->Init.BaudRate = 9600; // correct baud rate? also for above hal_uart_registercallback parameter why no parameters give for complete_isr?
+    handle->Init.WordLength = UART_WORDLENGTH_8B;
+    handle->Init.StopBits = UART_STOPBITS_1;
+    handle->Init.Parity = UART_PARITY_NONE; // are we using parity?
+    handle->Init.Mode = UART_MODE_TX_RX;    // why not defined?
+    handle->Init.HwFlowCtl = UART_HWCONTROL_NONE;
+    handle->Init.OverSampling = UART_OVERSAMPLING_16;
+    // What is the instance?? handle->Instance;
+
     uart_channel_map[device].handle = handle; // init this device's handle
 
     return status;
@@ -49,7 +64,8 @@ w_status_t uart_init(uart_channel_t device, UART_HandleTypeDef *handle) {
 // Write to the specified UART channel
 // One task can write to a channel at once, and concurrent calls will block for `timeout`
 // Returns the status of data transfer
-w_status_t uart_write(uart_channel_t channel, const uint8_t *data, uint8_t len, uint32_t timeout) {
+w_status_t uart_write(uart_channel_t channel, const uint8_t *data, uint8_t len, uint32_t timeout)
+{
     w_status_t status = W_SUCCESS;
 
     // TODO: call hal uart_transmit_IT ...
@@ -61,7 +77,8 @@ w_status_t uart_write(uart_channel_t channel, const uint8_t *data, uint8_t len, 
 // Will drop msgs if msgs are received faster than they are read
 // Blocks for up to `timeout` ms if no msg is available
 // Returns the status, received msg into `data`, msg length into `len`
-w_status_t uart_read(uart_channel_t channel, uint8_t *data, uint8_t *len, uint32_t timeout) {
+w_status_t uart_read(uart_channel_t channel, uint8_t *data, uint8_t *len, uint32_t timeout)
+{
     w_status_t status = W_SUCCESS;
     // aaaaaaaaaaaa
     return status;
