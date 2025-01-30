bef473077938c34435e003e395ca32f2c0aebb96
SAT-oO <82421061+SAT-oO@users.noreply.github.com>
2025-01-30 18:49:31 -0500

    updated header file according changes in design doc

--- src/application/controller/controller.h
+++ src/application/controller/controller.h
@@ -1,13 +1,16 @@
 #ifndef CONTROLLER_H_
 #define CONTROLLER_H_
 
+
+#include <stdbool.h>
+#include <stdlib.h>
+
 #include "FreeRTOS.h"
 #include "state_estimation.h"
-#include <stdbool.h>
 #include "flight_phase.h"
 #include "queue.h"
-
 #include "math.h"
+#include "common.h"
 
 
 
@@ -23,31 +26,27 @@ typedef struct {
     float timestamp; // Timestamp in ms
     float canard_coeff_CL; // Canard coefficient
     float canard_angle_delta; // Canard angle
-} controller_state_t;
+} controller_input_t;
 
 
 // main controller state using in task
 typedef struct {
-    controller_state_t current_state;
-    flight_phase_t last_error; 
+    controller_input_t current_state;
     bool controller_active;
     uint32_t last_ms;
     uint32_t can_send_errors;
     uint32_t data_miss_counter;
 } controller_t;
 
-// flight_conditions_t
+//flight_condition_t
 
-
-/* defined in state est.
-typedef struct{
-    float command_angle;
-    uint32_t timestamp;
-} control_output_SE_t; */
+//gain_table_t
 
 
 
-extern QueueHandle_t internalStateQueue;
+
+
+extern QueueHandle_t internalStateQueue; 
 
 
 /**
@@ -55,25 +54,30 @@ extern QueueHandle_t internalStateQueue;
  * Must be called before RTOS scheduler starts
  * @return true if initialization successful
  */
-bool controller_init(void);
+w_status_t controller_init(void);
 
 /**
  * Update controller with new state data - called by state estimation module
  * @param new_state Latest state estimate from state estimation
  * @return false if validation/queueing fails
  */
-bool controller_update_inputs(controller_state_t *new_state);
+w_status_t controller_update_inputs(controller_input_t *new_state);
 
 /**
  * Get most recent control output - called by state estimation module
  * @param output Pointer to store output -> type defined in state_estimation.h
  * @return false if no output available
  */
-bool controller_get_latest_output(control_output_SE_t *output);
+w_status_t controller_get_latest_output(control_output_SE_t *output);
 
 /**
  * Controller task function for RTOS
  */
 void controller_task(void *argument);
 
-#endif
+/*
+Send `canard_angle`, the desired canard angle (radians) to CAN
+*/
+w_status_t Controller_Send_Can(float canard_angle) 
+
+#endif // CONTROLLER_H_
