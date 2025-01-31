70bf91d67049ed405552481f543459594db26091
taisirhassan <taisirisawesome@gmail.com>
2025-01-31 02:53:19 +0000

    Remove I2C handle source and mock files from the project

--- src/drivers/i2c/i2c_handles.c
+++
@@ -1,5 +0,0 @@
-#include "i2c_handles.h"
-
-I2C_HandleTypeDef hi2c1;
-I2C_HandleTypeDef hi2c2;
-I2C_HandleTypeDef hi2c3;
--- tests/mocks/mock_i2c_handles.c
+++
@@ -1,6 +0,0 @@
-#include "mock_i2c_handles.h"
-
-/* Provide dummy definitions for the global I2C handles */
-I2C_HandleTypeDef hi2c1 = {0};
-I2C_HandleTypeDef hi2c2 = {0};
-I2C_HandleTypeDef hi2c3 = {0};
