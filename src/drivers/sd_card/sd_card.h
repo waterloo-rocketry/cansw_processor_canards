200ec236bef831d0341969e0497069262515d548
Wehbucci <wehbehasann@gmail.com>
2025-01-31 16:51:20 +0000

    Addressed PR comments: named my functions using snake_case, added comments, substituted UINT with uint32_t, made functions return the teamwide rocketlib status code instead of FRESULT

--- src/drivers/sd_card/sd_card.h
+++ src/drivers/sd_card/sd_card.h
@@ -1,18 +1,76 @@
 #ifndef SD_CARD_H
 #define SD_CARD_H
 
-#include <fatfs.h>
+/* C Standard Library */
+#include <stdint.h>
 
-FRESULT sdCardInit(void);
+/* Project-local headers */
+#include "rocketlib/include/common.h"
 
-FRESULT fileRead(char *fileName, void *buffer, UINT bufferSize, UINT *bytesRead);
+/**
+ * @brief Initialize the SD card hardware and create the mutex for thread safety.
+ *
+ * @return w_status_t - W_SUCCESS on success, or an appropriate error code.
+ */
+w_status_t sd_card_init(void);
 
-FRESULT fileWrite(char *fileName, void *buffer, UINT bufferSize, UINT *bytesWritten);
+/**
+ * @brief Read a file from the SD card.
+ *
+ * @param fileName - The name of the file to read.
+ * @param buffer - The buffer to read the file into.
+ * @param bufferSize - The size of the buffer.
+ * @param bytesRead - The number of bytes read from the file.
+ * @return w_status_t - W_SUCCESS on success, or an appropriate error code.
+ */
+w_status_t file_read(char *fileName, void *buffer, uint32_t bufferSize, uint32_t *bytesRead);
 
-FRESULT fileCreate(char *fileName);
+/**
+ * @brief Write data to a file on the SD card.
+ *
+ * Acquires the SD card mutex, opens the file, writes data from buffer,
+ * closes the file, then releases the mutex.
+ *
+ * @param[in]  file_name    Name/path of the file to write to.
+ * @param[in]  buffer       Pointer to the data to be written.
+ * @param[in]  buffer_size  Number of bytes from buffer to write.
+ * @param[out] bytes_written Actual number of bytes written (if successful).
+ *
+ * @return w_status_t - W_SUCCESS on success, or an appropriate error code.
+ */
+w_status_t file_write(char *fileName, void *buffer, uint32_t bufferSize, uint32_t *bytesWritten);
 
-FRESULT fileDelete(char *fileName);
+/**
+ * @brief Create a new file on the SD card.
+ *
+ * Acquires mutex, creates the file (if it does not exist),
+ * closes the file, then releases the mutex.
+ *
+ * @param[in] file_name  Name/path of the file to create.
+ *
+ * @return w_status_t - W_SUCCESS on success, or an appropriate error code.
+ */
+w_status_t file_create(char *fileName);
 
-FRESULT sdCardStatus(void);
+/**
+ * @brief Delete a file from the SD card.
+ *
+ * Acquires mutex, deletes the file, then releases the mutex.
+ *
+ * @param[in] file_name  Name/path of the file to delete.
+ *
+ * @return w_status_t - W_SUCCESS on success, or an appropriate error code.
+ */
+w_status_t file_delete(char *fileName);
+
+/**
+ * @brief Check SD card status or re-initialize as needed.
+ *
+ * Acquires mutex, performs any status checks or re-mounting,
+ * then releases the mutex.
+ *
+ * @return w_status_t - W_SUCCESS if the SD card is OK, or an error code otherwise.
+ */
+w_status_t sd_card_status(void);
 
 #endif // SD_CARD_H
