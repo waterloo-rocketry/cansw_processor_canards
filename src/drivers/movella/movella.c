1b0b8e9ab71f1d50b7bd49034d23a0dfe02a0ee0
celery6 <71736183+celery6@users.noreply.github.com>
2025-02-15 16:29:28 -0500

    add xsens-mti lib to cmake

--- src/drivers/movella/movella.c
+++ src/drivers/movella/movella.c
@@ -1,2 +1,8 @@
-#include "drivers/movella/movella.h"
+#include "FreeRTOS.h"
+#include "semphr.h"
+
+#include "stm32h7xx_hal.h"
 #include "third_party/xsens-mti/src/xsens_mti.h"
+
+#include "drivers/uart/uart.h"
+#include "drivers/movella/movella.h"
--- src/drivers/movella/movella.h
+++ src/drivers/movella/movella.h
@@ -2,15 +2,9 @@
 #define MOVELLA_H
 
 #include <stdint.h>
-#include <string.h>
 
-#include "FreeRTOS.h"
 #include "common/math/math.h"
-#include "semphr.h"
-#include "stm32h7xx_hal.h"
-#include "task.h"
 #include "third_party/rocketlib/include/common.h"
-#include "drivers/uart/uart.h"
 
 typedef struct {
     vector3d_t acc; // (x, y, z) m/s^2
@@ -19,15 +13,15 @@ typedef struct {
     vector3d_t mag; // (x, y, z) arbitrary units, note: need to be converted to uT later
     float pres; // Pa
     float temp; // °c
-} MovellaData_t;
+} movella_data_t;
 
-//Initialize the xsens interface and the UART module, pass the configuration to the sensor
+// Initialize the xsens interface, pass the configuration to the sensor
 w_status_t movella_init(void);
 
 // Resets the orientation
-void movella_reset_orientation(void);
+w_status_t movella_reset_orientation(void);
 
-// Return a copy structure of the MovellaData_t
-w_status_t movella_get_data(MovellaData_t *out_data);
+// Return a copy structure of the latest received movella_data_t
+w_status_t movella_get_data(movella_data_t *out_data);
 
 #endif
