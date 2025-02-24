f1606e96585ed9b6db2a1697dda90de6518749a1
Joe <joedolina@gmail.com>
2025-02-24 00:21:11 +0000

    Add altimu regmaps, add note to i2c, write WHOAMI function

--- src/drivers/altimu-10/altimu-10.c
+++ src/drivers/altimu-10/altimu-10.c
@@ -1,4 +1,8 @@
 #include "drivers/altimu-10/altimu-10.h"
+#include "drivers/altimu-10/LIS3MDL_regmap.h"
+#include "drivers/altimu-10/LPS_regmap.h"
+#include "drivers/altimu-10/LSM6DSO_regmap.h"
+#include "drivers/i2c/i2c.h"
 
 // AltIMU device addresses and configuration
 #define LSM6DSO_ADDR 0x6B // default (addr sel pin vdd)
@@ -6,18 +10,66 @@
 #define LPS22DF_ADDR 0x5D // default (addr sel pin vdd)
 
 /**
- * @brief Helper to read a sensor register.
- * @param reg_addr Register address to read from.
- * @param data Pointer to store the read data.
+ * @brief Performs a basic sanity check using the WHO_AM_I register.
  * @return Status of the operation.
  */
+w_status_t altimu_check_sanity(void) {
+    w_status_t i2c_status = W_SUCCESS;
+    w_status_t device_status = W_SUCCESS;
 
-// w_status_t altimu_read_register_with_timeout(uint8_t reg_addr, void *data);
+    const uint8_t expected_lis3mdl = 0x3D;
+    const uint8_t expected_lps22df = 0xB1;
+    const uint8_t expected_lsm6dso = 0x6C;
+
+    uint8_t who_am_i;
+    i2c_status |= i2c_read_reg(I2C_BUS_4, LIS3MDL_ADDR, LIS3_WHO_AM_I, &who_am_i, 1);
+    if (expected_lis3mdl != who_am_i) {
+        device_status |= W_FAILURE;
+    }
+
+    i2c_status |= i2c_read_reg(I2C_BUS_4, LPS22DF_ADDR, LPS_WHO_AM_I, &who_am_i, 1);
+    if (expected_lps22df != who_am_i) {
+        device_status |= W_FAILURE;
+    }
+
+    i2c_status |= i2c_read_reg(I2C_BUS_4, LSM6DSO_ADDR, WHO_AM_I_REG, &who_am_i, 1);
+    if (expected_lsm6dso != who_am_i) {
+        device_status |= W_FAILURE;
+    }
+
+    if (i2c_status != W_SUCCESS || device_status != W_SUCCESS) {
+        return W_FAILURE;
+    }
+    return W_SUCCESS;
+}
 
 /**
- * @brief Helper to write data to a sensor register.
- * @param reg_addr Register address to write to.
- * @param data Pointer to the data to write.
- * @return Status of the operation.
+ * @brief Initializes the Pololu AltIMU incl all register configs
+ * @note Must be called after scheduler start
+ * @return Status of the operation
  */
-// w_status_t altimu_write_register(uint8_t reg_addr, const void *data);
+w_status_t altimu_init();
+
+/**
+ * @brief Retrieves accelerometer data.
+ * @return Accelerometer data (gravities)
+ */
+w_status_t altimu_get_acc_data(vector3d_t *data);
+
+/**
+ * @brief Retrieves gyroscope data.
+ * @return Gyroscope data (deg/s)
+ */
+w_status_t altimu_get_gyro_data(vector3d_t *data);
+
+/**
+ * @brief Retrieves magnetometer data.
+ * @return Magnetometer data (gauss)
+ */
+w_status_t altimu_get_mag_data(vector3d_t *data);
+
+/**
+ * @brief Retrieves barometer data.
+ * @return Barometer data (pascal, celsius)
+ */
+w_status_t altimu_get_baro_data(altimu_barometer_data_t *data);
