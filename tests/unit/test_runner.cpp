bb56a5aa3c1b0626d26e74ae5b4ea67d3930627d
celery6 <71736183+celery6@users.noreply.github.com>
2025-03-01 22:42:24 -0500

    refactor unit test build setup to build tests individually

---
+++ tests/unit/test_runner.cpp
@@ -0,0 +1,9 @@
+#include "fff.h"
+#include <gtest/gtest.h>
+/**
+ * this file exists to provide any global definitions required by all tests.
+ * rn its just the FFF macro. idk if this will be useful for anything else
+ */
+
+// This macro must be called exactly 1 time in the entire test suite
+DEFINE_FFF_GLOBALS;
