67c1dc0179d5da4e490071f782fe3f8efc9621ff
celery <71736183+celery6@users.noreply.github.com>
2025-03-07 04:59:28 +0000

    init all existing modules in main.c

---
+++ src/application/can_handler/can_handler.c
@@ -0,0 +1,6 @@
+#include "application/can_handler/can_handler.h"
+
+// stub definition to compile
+w_status_t can_register_callback(can_msg_type_t msg_type, can_callback_t callback) {
+    return W_SUCCESS;
+};
--- src/application/logger/log.c
+++ src/application/logger/log.c
@@ -1,1 +1,6 @@
 #include "application/logger/log.h"
+
+// stub definition to compile
+w_status_t log_text(const char *source, const char *format, ...) {
+    return W_SUCCESS;
+}
