54790944beb67bcdf40ff7cb7430170fceb4724d
SAT-oO <z869liu@uwaterloo.ca>
2025-03-08 16:19:13 -0500

    Update src/application/controller/controller.c
    Co-authored-by: Joe <joedolina@gmail.com>

--- src/application/controller/controller.c
+++ src/application/controller/controller.c
@@ -47,7 +47,7 @@ w_status_t controller_init(void) {
     output_queue = xQueueCreate(1, sizeof(controller_output_t));
 
     // check queue creation
-    if (internal_state_queue == NULL || output_queue == NULL) {
+    if ((NULL == internal_state_queue) || (NULL == output_queue)) {
         init_status = W_FAILURE;
         log_text("controller", "queue creation failed");
     }
