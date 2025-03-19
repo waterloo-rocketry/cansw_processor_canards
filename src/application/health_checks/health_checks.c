16157d1a381c8c2568ed3c9a24ca9d5e30b392e0
Joe <joedolina@gmail.com>
2025-03-19 17:59:03 -0400

    Update task to do the thing

--- src/application/health_checks/health_checks.c
+++ src/application/health_checks/health_checks.c
@@ -1,38 +1,39 @@
-#include "FreeRTOS.h"
-#include "task.h"
 #include "health_checks.h"
-#include "drivers/adc/adc.h"
+#include "FreeRTOS.h"
 #include "application/can_handler/can_handler.h"
 #include "application/logger/log.h"
+#include "drivers/adc/adc.h"
+#include "drivers/timer/timer.h"
 #include "printf.h"
+#include "task.h"
 
-#define ADC_MAX_COUNTS 65535.0
-#define ADC_VREF 3.3
-#define R_SENSE 0.033
-#define INA180A3_GAIN 100.0
+#define TASK_DELAY_MS 1000
+
+#define ADC_MAX_COUNTS 65535.0f
+#define ADC_VREF 3.3f
+#define R_SENSE 0.033f
+#define INA180A3_GAIN 100.0f
 #define MAX_CURRENT_mA 400
 
 static w_status_t get_adc_current(uint16_t *adc_current_mA) {
-  uint32_t adc_value;
-  // TODO: get ADC values from adc_get_value()
-  // TODO: log and return result of adc value retrieval success/fail
- 
-  // TODO: get ADC constants from adc_get_constants();
-  // TODO: compare ADC value with ADC constants, log and return result
+    w_status_t status = W_SUCCESS;
+    uint32_t adc_value;
+    status |= adc_get_value(PROCESSOR_BOARD_VOLTAGE, &adc_value);
+    // TODO: log and return result of adc value retrieval success/fail
 
-  uint16_t voltage_mV =
-      (uint16_t)((((float)adc_value) / ADC_MAX_COUNTS) *
-                 ADC_VREF) *
-      1000;
-  *adc_current_mA =
-      (uint16_t)((voltage_mV / INA180A3_GAIN) / R_SENSE);
+    // TODO: get ADC constants from adc_get_constants();
+    // TODO: compare ADC value with ADC constants, log and return result
 
-  return W_SUCCESS;
+    uint16_t voltage_mV = (uint16_t)((((float)adc_value) / ADC_MAX_COUNTS) * ADC_VREF) *
+                          1000; // TODO this loses precision for no good reason
+    *adc_current_mA = (uint16_t)((voltage_mV / INA180A3_GAIN) / R_SENSE);
+
+    return W_SUCCESS;
 }
 
 w_status_t health_check_init(void) {
-  // TODO: initialize watchdog tasks
-  return W_SUCCESS;
+    // TODO: initialize watchdog tasks
+    return W_SUCCESS;
 }
 
 // TODO: void watchdog_kick(void)
@@ -40,34 +41,31 @@ w_status_t health_check_init(void) {
 // TODO: void watchdog_register_task(TaskHandle_t task_handle)
 
 void health_check_task(void *argument) {
-  TickType_t lastWakeTime = xTaskGetTickCount();
-  uint16_t adc_current_mA;
+    (void)argument;
+    TickType_t lastWakeTime = xTaskGetTickCount();
+    uint16_t adc_current_mA;
 
-  // TODO: get ADC values adc_constants_t constants = adc_get_constants();
+    // TODO: get ADC values adc_constants_t constants = adc_get_constants();
 
-  for (;;) {
-    if (get_adc_current(&adc_current_mA) == W_SUCCESS) {
-      if (adc_current_mA > MAX_CURRENT_mA) {
-        can_msg_t msg;
-        uint8_t current_data[2] = {
-            (adc_current_mA >> 8) & 0xFF, // High byte
-            adc_current_mA & 0xFF         // Low byte
-        };
+    for (;;) {
+        if (W_SUCCESS == get_adc_current(&adc_current_mA)) {
+            float ms = 0;
+            timer_get_ms(&ms);
+            can_msg_t msg = {0};
+            if (adc_current_mA > MAX_CURRENT_mA) {
+                build_general_board_status_msg(
+                    PRIO_HIGH, (uint16_t)ms, E_5V_OVER_CURRENT, adc_current_mA, &msg
+                );
+            } else {
+                build_general_board_status_msg(
+                    PRIO_LOW, (uint16_t)ms, E_NOMINAL, adc_current_mA, &msg
+                );
+            }
 
-        // TODO: build_msg()
-        // TODO: send CAN message 
-        if (can_handle_tx(&msg) != W_SUCCESS) {
-          // TODO: log if sent successfully
+            can_handler_transmit(&msg);
+            // TODO: monitor watchdog tasks
         }
-
-        // TODO: log if overcurrent detected
-      } else {
-        //TODO: log current draw
-      }
-
-      // TODO: monitor watchdog tasks
+        vTaskDelayUntil(&lastWakeTime, pdMS_TO_TICKS(TASK_DELAY_MS));
+        // TODO: check time doesn't overrun past 1000ms
     }
-    vTaskDelayUntil(&lastWakeTime, pdMS_TO_TICKS(1000));
-    // TODO: check time doesn't overrun past 1000ms
-  }
 }
