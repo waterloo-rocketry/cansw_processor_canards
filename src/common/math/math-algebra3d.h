65162860812ee9073ab41bd8d287f1aa4ac1d69e
Finn Breuer <finn@tcrobotics.eu>
2025-03-24 20:35:47 +0000

    finished math

--- src/common/math/math-algebra3d.h
+++ src/common/math/math-algebra3d.h
@@ -11,19 +11,22 @@
  * Rotation matrix transpose is inverse rotation
  */
 // vector * scalar | vector scaling
-vector3d_t math_vector_scale(float scalar, vector3d_t vector);
+vector3d_t math_vector_scale(float scalar, vector3d_t *vector);
 
 // vector + vector | vector addition
-vector3d_t math_vector_add(vector3d_t vector1, vector3d_t vector2);
+vector3d_t math_vector_add(const vector3d_t *vector1, const vector3d_t *vector2);
+
+// vector - vector | vector subtraction
+vector3d_t math_vector_subt(const vector3d_t *vector_left, const vector3d_t *vector_right);
 
 // vector * vector | vector dot product
-float math_vector_dot(vector3d_t vector1, vector3d_t vector2);
+float math_vector_dot(const vector3d_t *vector1, const vector3d_t *vector2);
 
 // vector x vector | vector cross product, only in R3
-vector3d_t math_vector_cross(vector3d_t vector_left, vector3d_t vector_right);
+vector3d_t math_vector_cross(const vector3d_t *vector_left, const vector3d_t *vector_right);
 
 // matrix * vector | matrix vector multiplication, for rotating vectors
-vector3d_t math_vector_rotate(matrix3d_t *matrix, vector3d_t vector_right);
+vector3d_t math_vector_rotate(const matrix3d_t *matrix, const vector3d_t *vector_right);
 
 // matrix ^T | transpose, for inverting rotation matrices
 matrix3d_t math_matrix_transp3(const matrix3d_t *input);
