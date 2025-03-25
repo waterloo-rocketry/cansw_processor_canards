680e7a9972d7190c3a05ac0c9e603891ca4e7aa0
Finn Breuer <finn@tcrobotics.eu>
2025-03-25 18:36:18 +0000

    Changed inputs to pointers, updated functions. Removed unneeded headers

--- src/application/estimator/model/quaternion.h
+++ src/application/estimator/model/quaternion.h
@@ -1,31 +1,31 @@
+/** 
+* Quaternion math operations
+*/
 #ifndef QUATERNION_H
 #define QUATERNION_H
 
-#include "application/estimator/estimator.h"
 #include "common/math/math.h"
-#include <stdbool.h>
-#include <stdint.h>
 #include <math.h>
 
 // // Norm of a quaternion
-float quaternion_norm(quaternion_t q);
+float quaternion_norm(const quaternion_t *q);
 
 // Normalize quaternion
-quaternion_t quaternion_normalize(quaternion_t q);
+quaternion_t quaternion_normalize(const quaternion_t *q);
 
 // Quaternion multiplication
-quaternion_t quaternion_multiply(quaternion_t q1, quaternion_t q2);
+quaternion_t quaternion_multiply(const quaternion_t *q1, const quaternion_t *q2);
 
 // Compute rotation matrix from a quaternion
-matrix3d_t quaternion_rotmatrix(quaternion_t q);
+matrix3d_t quaternion_rotmatrix(const quaternion_t *q);
 
 // Quaternion time derivative
-quaternion_t quaternion_derivative(quaternion_t q, vector3d_t omega);
+quaternion_t quaternion_derivative(const quaternion_t *q, const vector3d_t *omega);
 
 // Discrete update of quaternion differential equation
-quaternion_t quaternion_increment(quaternion_t q, vector3d_t omega, float deltaT);
+quaternion_t quaternion_increment(const quaternion_t *q, const vector3d_t *omega, float deltaT);
 
 // Compute Euler angles from a quaternion
-vector3d_t quaternion_to_euler(quaternion_t q);
+vector3d_t quaternion_to_euler(const quaternion_t *q);
 
 #endif
