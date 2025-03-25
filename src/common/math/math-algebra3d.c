dbd3d415f88443e89e2b83955d256fd614e3bc7f
Finn Breuer <finn@tcrobotics.eu>
2025-03-25 17:19:45 +0000

    fixed requested changes

--- src/common/math/math-algebra3d.c
+++ src/common/math/math-algebra3d.c
@@ -6,31 +6,31 @@
 
 /**
  * Special operations in R3, as they are used often
- * Cross product only exists in R3 
+ * Cross product only exists in R3
  * Rotation matrix transpose is inverse rotation
  */
-// vector * scalar | vector scaling
-vector3d_t math_vector_scale(float scalar, const vector3d_t *vector)
+// vector * scalar // vector scaling
+vector3d_t math_vector3d_scale(float scalar, const vector3d_t *vector)
 {
     vector3d_t result;
     result.x = scalar * vector->x;
-    result.y = scalar * vector->y;
+    result.y = scalar *vector->y;
     result.z = scalar * vector->z;
     return result;
 }
 
-// vector + vector | vector addition
-vector3d_t math_vector_add(const vector3d_t *vector1, const vector3d_t *vector2)
+// vector + vector // vector addition
+vector3d_t math_vector3d_add(const vector3d_t *vector1, const vector3d_t *vector2)
 {
     vector3d_t result;
-    result.x = vector1->x + vector2->x;
+    result.x = vector1->x+vector2->x;
     result.y = vector1->y + vector2->y;
     result.z = vector1->z + vector2->z;
     return result;
 }
 
-// vector - vector | vector subtraction
-vector3d_t math_vector_subt(const vector3d_t *vector_left, const vector3d_t *vector_right)
+// vector - vector // vector subtraction
+vector3d_t math_vector3d_subt(const vector3d_t *vector_left, const vector3d_t *vector_right)
 {
     vector3d_t result;
     result.x = vector_left->x - vector_right->x;
@@ -39,16 +39,16 @@ vector3d_t math_vector_subt(const vector3d_t *vector_left, const vector3d_t *vec
     return result;
 }
 
-// vector * vector | vector dot product
-float math_vector_dot(const vector3d_t *vector1, const vector3d_t *vector2)
+// vector * vector // vector dot product
+float math_vector3d_dot(const vector3d_t *vector1, const vector3d_t *vector2)
 {
     float result;
     result = (vector1->x * vector2->x) + (vector1->y * vector2->y) + (vector1->z * vector2->z);
     return result;
 }
 
-// vector x vector | vector cross product, only in R3
-vector3d_t math_vector_cross(const vector3d_t *vector_left, const vector3d_t *vector_right)
+// vector x vector // vector cross product, only in R3
+vector3d_t math_vector3d_cross(const vector3d_t *vector_left, const vector3d_t *vector_right)
 {
     vector3d_t result;
     result.x = vector_left->y * vector_right->z - vector_left->z * vector_right->y;
@@ -57,18 +57,8 @@ vector3d_t math_vector_cross(const vector3d_t *vector_left, const vector3d_t *ve
     return result;
 }
 
-/* // matrix * vector | matrix vector multiplication, for rotating vectors
-vector3d_t math_vector_rotate(const matrix3d_t &matrix, const vector3d_t &vector_right)
-{
-    vector3d_t result;
-    result.x = matrix.s11 * vector.x + matrix.s12 * vector.y + matrix.s13 * vector.z;
-    result.y = matrix.s21 * vector.x + matrix.s22 * vector.y + matrix.s23 * vector.z;
-    result.z = matrix.s31 * vector.x + matrix.s32 * vector.y + matrix.s33 * vector.z;
-    return result;
-} */
-
-// matrix * vector | matrix vector multiplication, for rotating vectors
-vector3d_t math_vector_rotate(const matrix3d_t *matrix, const vector3d_t *vector_right)
+// matrix * vector // matrix vector multiplication, for rotating vectors
+vector3d_t math_vector3d_rotate(const matrix3d_t *matrix, const vector3d_t *vector_right)
 {
     vector3d_t result;
     result->x = matrix->s11 * vector->x + matrix->s12 * vector->y + matrix->s13 * vector->z;
@@ -77,8 +67,8 @@ vector3d_t math_vector_rotate(const matrix3d_t *matrix, const vector3d_t *vector
     return result;
 }
 
-// matrix ^T | transpose, for inverting rotation matrices
-matrix3d_t math_matrix_transp3(const matrix3d_t *input)
+// matrix ^T // transpose, for inverting rotation matrices
+matrix3d_t math_matrix3d_transp(const matrix3d_t *input)
 {
     matrix3d_t result;
     result.s11 = input->s11; result.s12 = input->s21; result.s13 = input->s31;
--- src/common/math/math-algebra3d.h
+++ src/common/math/math-algebra3d.h
@@ -7,28 +7,28 @@
 
 /**
  * Special operations in R3, as they are used often
- * Cross product only exists in R3 
+ * Cross product only exists in R3
  * Rotation matrix transpose is inverse rotation
  */
-// vector * scalar | vector scaling
-vector3d_t math_vector_scale(float scalar, vector3d_t *vector);
+// vector * scalar // vector scaling
+vector3d_t math_vector3d_scale(float scalar, vector3d_t *vector);
 
-// vector + vector | vector addition
-vector3d_t math_vector_add(const vector3d_t *vector1, const vector3d_t *vector2);
+// vector + vector // vector addition
+vector3d_t math_vector3d_add(const vector3d_t *vector1, const vector3d_t *vector2);
 
-// vector - vector | vector subtraction
-vector3d_t math_vector_subt(const vector3d_t *vector_left, const vector3d_t *vector_right);
+// vector - vector // vector subtraction
+vector3d_t math_vector3d_subt(const vector3d_t *vector_left, const vector3d_t *vector_right);
 
-// vector * vector | vector dot product
-float math_vector_dot(const vector3d_t *vector1, const vector3d_t *vector2);
+// vector * vector // vector dot product
+float math_vector3d_dot(const vector3d_t *vector1, const vector3d_t *vector2);
 
-// vector x vector | vector cross product, only in R3
-vector3d_t math_vector_cross(const vector3d_t *vector_left, const vector3d_t *vector_right);
+// vector x vector // vector cross product, only in R3
+vector3d_t math_vector3d_cross(const vector3d_t *vector_left, const vector3d_t *vector_right);
 
-// matrix * vector | matrix vector multiplication, for rotating vectors
-vector3d_t math_vector_rotate(const matrix3d_t *matrix, const vector3d_t *vector_right);
+// matrix * vector // matrix vector multiplication, for rotating vectors
+vector3d_t math_vector3d_rotate(const matrix3d_t *matrix, const vector3d_t *vector_right);
 
-// matrix ^T | transpose, for inverting rotation matrices
-matrix3d_t math_matrix_transp3(const matrix3d_t *input);
+// matrix ^T // transpose, for inverting rotation matrices
+matrix3d_t math_matrix3d_transp(const matrix3d_t *input);
 
 #endif
