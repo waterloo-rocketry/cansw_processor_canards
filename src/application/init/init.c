7952fe8a4d29bfcc388be88635e5407373612d42
Yuvraj Birdi <ysbirdi@uwaterloo.ca>
2025-04-04 18:15:24 +0000

    trying to fix the formatting error's I don't even know why it's not working

--- src/application/init/init.c
+++ src/application/init/init.c
@@ -14,9 +14,9 @@
 #include "drivers/uart/uart.h"
 #include "stm32h7xx_hal.h"
 // Add these includes for hardware handles
-#include "adc.h" // For hadc1
+#include "adc.h"   // For hadc1
 #include "fdcan.h" // For hfdcan1
-#include "i2c.h" // For hi2c2, hi2c4
+#include "i2c.h"   // For hi2c2, hi2c4
 #include "usart.h" // For huart4, huart8
 
 // Initialize task handles to NULL
@@ -33,7 +33,8 @@ TaskHandle_t movella_task_handle = NULL;
 // Task priorities
 // flight phase must have highest priority to preempt everything else
 const uint32_t flight_phase_task_priority = configMAX_PRIORITIES - 1;
-// TODO: replace with actual priorities once determined. for now just make all same priority
+// TODO: replace with actual priorities once determined. for now just make all
+// same priority
 const uint32_t log_task_priority = configMAX_PRIORITIES - 5;
 const uint32_t estimator_task_priority = configMAX_PRIORITIES - 5;
 const uint32_t controller_task_priority = configMAX_PRIORITIES - 5;
@@ -45,130 +46,97 @@ const uint32_t movella_task_priority = configMAX_PRIORITIES - 5;
 
 // Initialize a function with retry logic
 w_status_t init_with_retry(w_status_t (*init_fn)(void)) {
-    w_status_t status;
-    uint32_t retry_count = 0;
+  w_status_t status;
+  uint32_t retry_count = 0;
 
-    while (retry_count < MAX_INIT_RETRIES) {
-        status = init_fn();
+  while (retry_count < MAX_INIT_RETRIES) {
+    status = init_fn();
 
-        if (status == W_SUCCESS) {
-            return W_SUCCESS;
-        }
-
-        retry_count++;
-        if (retry_count < MAX_INIT_RETRIES) {
-            HAL_Delay(INIT_RETRY_DELAY_MS);
-        }
+    if (status == W_SUCCESS) {
+      return W_SUCCESS;
     }
 
-    return W_FAILURE;
+    retry_count++;
+    if (retry_count < MAX_INIT_RETRIES) {
+      HAL_Delay(INIT_RETRY_DELAY_MS);
+    }
+  }
+
+  return W_FAILURE;
 }
 
 // Initialize a function with retry logic and parameter
 w_status_t init_with_retry_param(w_status_t (*init_fn)(void *), void *param) {
-    w_status_t status;
-    uint32_t retry_count = 0;
+  w_status_t status;
+  uint32_t retry_count = 0;
 
-    while (retry_count < MAX_INIT_RETRIES) {
-        status = init_fn(param);
+  while (retry_count < MAX_INIT_RETRIES) {
+    status = init_fn(param);
 
-        if (status == W_SUCCESS) {
-            return W_SUCCESS;
-        }
-
-        retry_count++;
-        if (retry_count < MAX_INIT_RETRIES) {
-            HAL_Delay(INIT_RETRY_DELAY_MS);
-        }
+    if (status == W_SUCCESS) {
+      return W_SUCCESS;
     }
 
-    return W_FAILURE;
+    retry_count++;
+    if (retry_count < MAX_INIT_RETRIES) {
+      HAL_Delay(INIT_RETRY_DELAY_MS);
+    }
+  }
+
+  return W_FAILURE;
 }
 
 // Main initialization function
 w_status_t system_init(void) {
-    w_status_t status = W_SUCCESS;
+  w_status_t status = W_SUCCESS;
 
-    // Initialize hardware peripherals
-    status |= gpio_init();
-    status |= i2c_init(I2C_BUS_2, &hi2c2, 0);
-    status |= i2c_init(I2C_BUS_4, &hi2c4, 0);
-    status |= uart_init(UART_DEBUG_SERIAL, &huart4, 100);
-    status |= uart_init(UART_MOVELLA, &huart8, 100);
-    status |= adc_init(&hadc1);
+  // Initialize hardware peripherals
+  status |= gpio_init();
+  status |= i2c_init(I2C_BUS_2, &hi2c2, 0);
+  status |= i2c_init(I2C_BUS_4, &hi2c4, 0);
+  status |= uart_init(UART_DEBUG_SERIAL, &huart4, 100);
+  status |= uart_init(UART_MOVELLA, &huart8, 100);
+  status |= adc_init(&hadc1);
 
-    // Initialize application modules with retry logic
-    status |= init_with_retry(altimu_init);
-    status |= init_with_retry(movella_init);
-    status |= init_with_retry(flight_phase_init);
-    status |= init_with_retry(imu_handler_init);
-    status |= init_with_retry_param((w_status_t(*)(void *))can_handler_init, &hfdcan1);
-    status |= init_with_retry(controller_init);
+  // Initialize application modules with retry logic
+  status |= init_with_retry(altimu_init);
+  status |= init_with_retry(movella_init);
+  status |= init_with_retry(flight_phase_init);
+  status |= init_with_retry(imu_handler_init);
+  status |=
+      init_with_retry_param((w_status_t(*)(void *))can_handler_init, &hfdcan1);
+  status |= init_with_retry(controller_init);
 
-    if (status != W_SUCCESS) {
-        return status;
-    }
+  if (status != W_SUCCESS) {
+    return status;
+  }
 
-    // Create FreeRTOS tasks
-    BaseType_t task_status = pdTRUE;
+  // Create FreeRTOS tasks
+  BaseType_t task_status = pdTRUE;
 
-    task_status &= xTaskCreate(
-        flight_phase_task,
-        "flight phase",
-        512,
-        NULL,
-        flight_phase_task_priority,
-        &flight_phase_task_handle
-    );
+  task_status &=
+      xTaskCreate(flight_phase_task, "flight phase", 512, NULL,
+                  flight_phase_task_priority, &flight_phase_task_handle);
 
-    task_status &= xTaskCreate(
-        imu_handler_task,
-        "imu handler",
-        512,
-        NULL,
-        imu_handler_task_priority,
-        &imu_handler_task_handle
-    );
+  task_status &=
+      xTaskCreate(imu_handler_task, "imu handler", 512, NULL,
+                  imu_handler_task_priority, &imu_handler_task_handle);
 
-    task_status &= xTaskCreate(
-        can_handler_task_rx,
-        "can handler rx",
-        512,
-        NULL,
-        can_handler_rx_priority,
-        &can_handler_handle_rx
-    );
+  task_status &= xTaskCreate(can_handler_task_rx, "can handler rx", 512, NULL,
+                             can_handler_rx_priority, &can_handler_handle_rx);
 
-    task_status &= xTaskCreate(
-        can_handler_task_tx,
-        "can handler tx",
-        512,
-        NULL,
-        can_handler_tx_priority,
-        &can_handler_handle_tx
-    );
+  task_status &= xTaskCreate(can_handler_task_tx, "can handler tx", 512, NULL,
+                             can_handler_tx_priority, &can_handler_handle_tx);
 
-    task_status &= xTaskCreate(
-        movella_task,
-        "movella",
-        2560,
-        NULL,
-        movella_task_priority,
-        &movella_task_handle
-    );
+  task_status &= xTaskCreate(movella_task, "movella", 2560, NULL,
+                             movella_task_priority, &movella_task_handle);
 
-    task_status &= xTaskCreate(
-        log_task,
-        "logger",
-        2048,
-        NULL,
-        log_task_priority,
-        &log_task_handle
-    );
+  task_status &= xTaskCreate(log_task, "logger", 2048, NULL, log_task_priority,
+                             &log_task_handle);
 
-    if (task_status != pdTRUE) {
-        return W_FAILURE;
-    }
+  if (task_status != pdTRUE) {
+    return W_FAILURE;
+  }
 
-    return W_SUCCESS;
+  return W_SUCCESS;
 }
