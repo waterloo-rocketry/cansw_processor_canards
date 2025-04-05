6e8dde323f337c74ec7a8371259aa1db3dad03e7
SAT-oO <z869liu@uwaterloo.ca>
2025-04-05 20:36:46 +0000

    setup for andy

---
+++ src/application/estimator/model/model_imu.c
@@ -0,0 +1,16 @@
+// include paths
+#include "application/estimator/model/model_imu.h"
+
+// define constants (if applicable)
+
+
+
+
+
+// define instances 
+estimator_state_t estimator_state __attribute__((unused)) = {0};
+estimator_imu_data_t estimator_imu_data __attribute__((unused)) = {0};
+
+
+// define functions
+
--- src/application/estimator/model/model_imu.h
+++ src/application/estimator/model/model_imu.h
@@ -12,12 +12,22 @@ typedef union {
         float altitude;
         float CL;
         float delta;
-    }
+    };
 } estimator_state_t;
 
+// sensor data from 1 arbitrary imu
+typedef union {
+    float array[10];
+    struct {
+        vector3d_t accelerometer;
+        vector3d_t gyroscope;
+        vector3d_t magnetometer;
+        float barometer;
+    };
+} estimator_imu_data_t;
 
-
-
+estimator_imu_data_t model_measurement_imu_mti630(estimator_state_t *est_state, estimator_imu_data_t *imu_bias);
+estimator_imu_data_t model_measurement_imu_altimu(estimator_state_t *est_state, estimator_imu_data_t *imu_bias);
 
 
 #endif 
