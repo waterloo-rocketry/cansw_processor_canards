5a024fcbc19be054c5ce36b000d29c0041039b29
Finn Breuer <finn@tcrobotics.eu>
2025-04-06 00:18:54 +0000

    format #2

--- src/application/estimator/estimator_types.h
+++ src/application/estimator/estimator_types.h
@@ -7,8 +7,8 @@
 #include "common/math/math.h"
 
 /*
-* State
-*/ 
+ * State
+ */
 typedef union {
     float array[13];
     struct __attribute__((packed)) {
@@ -22,23 +22,23 @@ typedef union {
 } x_state_t;
 
 /*
-* Input signal
-*/ 
+ * Input signal
+ */
 typedef struct {
     float cmd;
     vector3d_t acceleration;
 } u_dynamics_t;
 
 /*
-* Measurement signals
-*/ 
+ * Measurement signals
+ */
 // sensor data from 1 arbitrary imu
 // Pad filter: both filtered_i and bias_i can be of this type
 // EKF: bias_i, y_i, and h_x_i are of this type
 // IMU measurement model: the return is of this type
 typedef union {
     float array[10];
-    struct  __attribute__((packed)) {
+    struct __attribute__((packed)) {
         vector3d_t accelerometer;
         vector3d_t gyroscope;
         vector3d_t magnetometer;
