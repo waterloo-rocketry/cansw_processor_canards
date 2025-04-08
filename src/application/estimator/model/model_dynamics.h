5bc28bd9d275d946a34c6bf984db170ed242e139
SAT-oO <z869liu@uwaterloo.ca>
2025-04-08 19:00:02 +0000

    i need to switch branch :skull

--- src/application/estimator/model/model_dynamics.h
+++ src/application/estimator/model/model_dynamics.h
@@ -1,6 +1,7 @@
 #ifndef MODEL_DYNAMICS_H
 #define MODEL_DYNAMICS_H
 
+#include "application/estimator/estimator_types.h"
 #include "application/estimator/model/quaternion.h"
 #include "application/estimator/model/model_airdata.h"
 #include "application/estimator/estimator.h"
@@ -10,27 +11,12 @@
 #include <stdint.h>
 #include <math.h>
 
-// definitions are here temporarily for easy lookup
-typedef union {
-    float array[13];
-    struct {
-        quaternion_t attitude;  // Attitude quaternion
-        vector3d_t rates;       // Angular rates, body frame
-        vector3d_t velocity;    // Velocity vector, body frame
-        float altitude;         // Altitude
-        float CL;               // Canard coefficient
-        float delta;            // Canard angle
-    };
-}estimator_state_t;
 
-typedef struct {
-    vector3d_t acceleration; // Averaged specific force, from acceleration model
-    float canard_command;    // Canard angle
-} estimator_input_t;
+x_state_t model_dynamics_update(x_state_t *state, u_dynamics_t *input, uint32_t timestamp);
 
-estimator_state_t model_dynamics_update(float dt, estimator_state_t *est_state, estimator_input_t *est_input);
 
-void model_dynamics_jacobian(float dynamics_jacobian, float dt, estimator_state_t *state, estimator_input_t *input);
+void model_dynamics_jacobian(arm_matrix_instance_f32 *dynamics_jacobian, x_state_t *state, u_dynamics_t *input, uint32_t timestamp);
+
 
 // void model_dynamics_weights(float *dynamics_weights);
 
