b61111ac1f76b7ed8492c9bab68dd7c7dd8f74bf
SAT-oO <z869liu@uwaterloo.ca>
2025-04-15 19:17:27 +0000

    formatting

--- src/application/estimator/model/model_airdata.h
+++ src/application/estimator/model/model_airdata.h
@@ -21,7 +21,7 @@ estimator_airdata_t model_airdata(double altitude);
 /*
  * Jacobian of the airdata function (partial derivative of sub-function)
  * @param double altitude
- * @return double pressure change with respect to altitude 
+ * @return double pressure change with respect to altitude
  */
 
 double model_airdata_jacobian(double altitude);
