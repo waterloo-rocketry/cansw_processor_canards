48cf3cb8ca451c363c33c34a4f295d98999e8c9e
Andy-Li308 <andy.li1365@gmail.com>
2025-04-16 17:10:50 -0400

    removed extra types in model_imu.h

--- src/application/estimator/model/model_imu.h
+++ src/application/estimator/model/model_imu.h
@@ -3,28 +3,6 @@
 
 #include "application/estimator/estimator_types.h"
 #include "common/math/math.h"
-typedef union {
-    float array[13];
-    struct {
-        quaternion_t attitude;
-        vector3d_t rates;
-        vector3d_t velocity;
-        float altitude;
-        float CL;
-        float delta;
-    };
-} est_state_t;
-
-// sensor data from 1 arbitrary imu
-typedef union {
-    float array[10];
-    struct {
-        vector3d_t accelerometer;
-        vector3d_t gyroscope;
-        vector3d_t magnetometer;
-        float barometer;
-    };
-} est_imu_data_t;
 
 y_imu_t model_measurement_imu_(x_state_t *est_state, y_imu_t *imu_bias);
 
