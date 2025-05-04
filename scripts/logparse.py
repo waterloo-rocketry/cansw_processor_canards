4ec4349f32554a74818db5599904575b0f990c7d
celery6 <71736183+celery6@users.noreply.github.com>
2025-05-04 04:24:43 -0400

    revert

--- scripts/logparse.py
+++ scripts/logparse.py
@@ -72,16 +72,6 @@ FORMATS = {
 
 }
 
-ACC_FS = 16.0 / 32767      # g / LSB
-GYRO_FS = 2000.0 / 32767   # dps / LSB
-MAG_FS = 16.0 / 32767      # gauss / LSB
-BARO_FS = 100.0 / 4096.0   # Pa / LSB
-TEMP_FS = 1.0 / 100.0      # °C / LSB
-
-def int16(val):
-    """Convert raw unsigned 16-bit to signed"""
-    return val if val < 0x8000 else val - 0x10000
-
 def parse_argv(argv):
     p = argparse.ArgumentParser()
     p.add_argument("infile")
@@ -94,42 +84,9 @@ def main(argv=None):
     # Loop through message regions
     for pos in range(0, len(data), MAX_MSG_DATA_LENGTH):
         type_int, timestamp = struct.unpack_from("<Lf", data, pos)
-
         print(f"[{timestamp:.1f}] ", end="")
-        spec = FORMATS[type_int]
-
-        # convert pololu
-        if type_int == M(0x08):
-            raw_vals = struct.unpack_from(spec.format, data, pos + 8)
-
-            acc = [int16(raw_vals[0]) * ACC_FS,
-                int16(raw_vals[1]) * ACC_FS,
-                int16(raw_vals[2]) * ACC_FS]
-
-            gyro = [int16(raw_vals[3]) * GYRO_FS,
-                    int16(raw_vals[4]) * GYRO_FS,
-                    int16(raw_vals[5]) * GYRO_FS]
-
-            mag = [int16(raw_vals[6]) * MAG_FS,
-                int16(raw_vals[7]) * MAG_FS,
-                int16(raw_vals[8]) * MAG_FS]
-
-            baro = raw_vals[9] * BARO_FS
-            temp = int16(raw_vals[10]) * TEMP_FS
-
-            print(f"    acc_x: {acc[0]:.3f} g")
-            print(f"    acc_y: {acc[1]:.3f} g")
-            print(f"    acc_z: {acc[2]:.3f} g")
-            print(f"    gyro_x: {gyro[0]:.2f} °/s")
-            print(f"    gyro_y: {gyro[1]:.2f} °/s")
-            print(f"    gyro_z: {gyro[2]:.2f} °/s")
-            print(f"    mag_x: {mag[0]:.3f} G")
-            print(f"    mag_y: {mag[1]:.3f} G")
-            print(f"    mag_z: {mag[2]:.3f} G")
-            print(f"    baro_pressure: {baro:.1f} Pa")
-            print(f"    baro_temp: {temp:.2f} °C")
-
         try:
+            spec = FORMATS[type_int]
             print(f"{spec.name} (type ", end="")
             if spec.name != "header":
                 print(type_int >> 16, end="")
